b85f589bf908b19c726d7b2f1805a16e9875e278
westondcrewe <westondcrewe@gmail.com>
2024-02-13 21:51:30 -0800

    just realized I was making bar plots not line plots

--- src/alternative_reduce.py
+++ src/alternative_reduce.py
@@ -39,18 +39,23 @@ for output in sorted(os.listdir("./outputs")):
 # visualize
 
 # print the count values
+hashtag_string = ''
 for hashtag in args.hashtags:
-    print('hashtag =', hashtag)
+    hashtag_string += hashtag
+    hashtag_string += '_'
     items = sorted(total[hashtag].items(), key=lambda item: datetime.strptime(item[0], "%y-%m-%d"))
     for k,v in items:
         print(k,':',v)
     tweets = [i[1] for i in items]
     date = [datetime.strptime(i[0], "%y-%m-%d") for i in items]
-    plt.bar(date, tweets)
-    plt.xlabel("Date")
-    plt.gca().xaxis.set_major_locator(mdates.MonthLocator(bymonthday=1))
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%y-%m-%d"))
-    plt.xticks([x for x in date if x.day == 1], rotation=45)
-    plt.ylabel(f"Number of Tweets with {hashtag}")
-    plt.tight_layout(pad = 1.15)
-    plt.savefig(f"daily_{hashtag}_tweets_plot.png")
+    plt.plot(date, tweets, label = hashtag)
+#date = [datetime.strptime(i[0], "%y-%m-%d") for i in total[args.hashtags[0]]]
+plt.xlabel("Date")
+plt.gca().xaxis.set_major_locator(mdates.MonthLocator(bymonthday=1))
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%y-%m-%d"))
+#plt.xticks([x for x in date if x.day == 1], rotation=45)
+plt.xticks(rotation = 45)
+plt.ylabel("Number of Tweets")
+plt.tight_layout(pad = 1.15)
+plt.legend()
+plt.savefig(f"daily_{hashtag_string}tweets_plot.png")
